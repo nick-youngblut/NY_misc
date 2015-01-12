df4055f48cb9cfd55ec29eeca93fe5b9be46913b
Nick Youngblut <ndy2@cornell.edu>
2015-01-12 14:54:52 -0700

    updated 96well

--- bin/96well.py
+++ bin/96well.py
@@ -22,16 +22,18 @@ import string
 
 if __name__ == '__main__':
     args = docopt(__doc__, version='0.1')
+    args['-n'] = int(args['-n'])
 
     letters = list(string.uppercase)[:8] * 12
     nums = [x for x in range(13)[1:] for y in range(8)]
     well_IDs = [''.join([x,str(y)]) for (x,y) in zip(letters, nums)]
     
     if args['--wide']:
+        well_IDs = [well_IDs[i] if i <= args['-n'] else '' for i in xrange(len(well_IDs))]
         for x in xrange(8):
             print '\t'.join(well_IDs[x::8])
     else:    
-        for i in xrange(int(args['-n'])-1):
+        for i in xrange(args['-n']):
             print well_IDs[i]
 
     
